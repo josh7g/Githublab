d5ebdc35089f5641d4b6a8166c5fb147d2b74131
joshuanketsiah7 <64086242+josh7g@users.noreply.github.com>
2024-11-26 13:14:18 +0000

    Add files via upload

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSONB
 
 db = SQLAlchemy()
 
