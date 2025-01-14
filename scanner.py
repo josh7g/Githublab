03f66ddb3ea84fdc27a35d7ded893f8a95eed378
joshuanketsiah7 <64086242+josh7g@users.noreply.github.com>
2025-01-14 14:02:48 +0000

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -253,7 +253,7 @@ class SecurityScanner:
             cmd = [
                 "semgrep",
                 "scan",
-                "--config", "p/owasp-top-ten",
+                "--config", "p/security-audit", "p/owasp-top-ten",
                 "--json",
                 "--verbose",
                 "--metrics=on",
